66efe15de2f71b4cad8dfab3a66b51cb2ac77b3a
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-02-09 22:34:31 -0300

    versao inicial do login

---
+++ pages/api/login.ts
@@ -0,0 +1,17 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+
+export default (
+    req : NextApiRequest,
+    res : NextApiResponse
+) => {
+    if(req.method === 'POST'){
+        const {login, senha} = req.body;
+
+        if(login === 'admin@admin.com' && 
+            senha === 'Admin@123'){
+                res.status(200).json({msg : 'Usuario autenticado com sucesso'});
+        }
+        return res.status(405).json({erro : 'Usuario ou senha nao encontrado'});
+    }
+    return res.status(405).json({erro : 'Metodo informado nao e valido'});
+}
