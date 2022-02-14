323c5c36a29f29827a0bd8c1a47d715d4971139d
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-02-14 20:18:33 -0300

    Update login.ts

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -11,7 +11,7 @@ export default (
             senha === 'Admin@123'){
                 res.status(200).json({msg : 'Usuario autenticado com sucesso'});
         }
-        return res.status(405).json({erro : 'Usuario ou senha nao encontrado'});
+        return res.status(400).json({erro : 'Usuario ou senha nao encontrado'});
     }
     return res.status(405).json({erro : 'Metodo informado nao e valido'});
 }
