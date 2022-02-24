5de1cdbc9b9f4f4abf411f9fb6345440a76e98c6
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-02-23 21:53:54 -0300

    conclusao api de comentarios

---
+++ pages/api/comentario.ts
@@ -0,0 +1,45 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
+import { validarTokenJWT } from '../../middlewares/validarTokenJWT';
+import { PublicacaoModel } from '../../models/PublicacaoModel';
+import { UsuarioModel } from '../../models/UsuarioModel';
+import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
+
+const comentarioEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg>) => {
+    try{
+        if(req.method === 'PUT'){
+            const {userId, id} = req.query;
+            const usuarioLogado = await UsuarioModel.findById(userId);
+            if(!usuarioLogado){
+                return res.status(400).json({erro : 'Usuario nao encontrado'});
+            }
+            
+            const publicacao =  await PublicacaoModel.findById(id);
+            if(!publicacao){
+                return res.status(400).json({erro : 'Publicacao nao encontrada'});
+            }
+
+            if(!req.body || !req.body.comentario
+                || req.body.comentario.length < 2){
+                return res.status(400).json({erro : 'Comentario nao e valido'});
+            }
+
+            const comentario = {
+                usuarioId : usuarioLogado._id,
+                nome : usuarioLogado.nome,
+                comentario : req.body.comentario
+            }
+
+            publicacao.comentarios.push(comentario);
+            await PublicacaoModel.findByIdAndUpdate({_id : publicacao._id}, publicacao);
+            return res.status(200).json({msg : 'Comentario adicionado com sucesso'});
+        }
+        
+        return res.status(405).json({erro : 'Metodo informado nao e valido'});
+    }catch(e){
+        console.log(e);
+        return res.status(500).json({erro : 'Ocorreu erro ao adicionar comentario'});
+    }
+}
+
+export default validarTokenJWT(conectarMongoDB(comentarioEndpoint));
