53003aac22485c64870678fb7681754769a16d11
Rafael Thomazelli <rafael.thomazelli@gmail.com>
2022-04-27 13:51:13 -0300

    formatando o usuario

--- pages/api/pesquisa.ts
+++ pages/api/pesquisa.ts
@@ -15,14 +15,26 @@ const pesquisaEndpoint
                     if (!usuarioEncontrado) {
                         return res.status(400).json({ erro: 'Usuario nao encontrado' });
                     }
+
+                    const user = {
+                        senha: null,
+                        segueEsseUsuario: false,
+                        nome: usuarioEncontrado.nome,
+                        email: usuarioEncontrado.email,
+                        _id: usuarioEncontrado._id,
+                        avatar: usuarioEncontrado.avatar,
+                        seguidores: usuarioEncontrado.seguidores,
+                        seguindo: usuarioEncontrado.seguindo,
+                        publicacoes: usuarioEncontrado.publicacoes,
+                    } as any;
+
                     const segueEsseUsuario = await SeguidorModel.find({ usuarioId: req?.query?.userId, usuarioSeguidoId: usuarioEncontrado._id });
                     if (segueEsseUsuario && segueEsseUsuario.length > 0) {
-                        usuarioEncontrado.segueEsseUsuario = true;
-                    }else{
-                        usuarioEncontrado.segueEsseUsuario = false;
+                        user.segueEsseUsuario = true;
                     }
-                    usuarioEncontrado.senha = null;
-                    return res.status(200).json(usuarioEncontrado);
+                    user.senha = null;
+
+                    return res.status(200).json(user);
                 } else {
                     const { filtro } = req.query;
                     if (!filtro || filtro.length < 2) {
@@ -35,16 +47,27 @@ const pesquisaEndpoint
                         ]
                     });
 
-                    usuariosEncontrados.forEach(async userFound => {
+                   const users = usuariosEncontrados.map(async userFound => {
                         const segueEsseUsuario = await SeguidorModel.find({ usuarioId: req?.query?.userId, usuarioSeguidoId: userFound._id });
+                        const user = {
+                            senha: null,
+                            segueEsseUsuario: false,
+                            nome: userFound.nome,
+                            email: userFound.email,
+                            _id: userFound._id,
+                            avatar: userFound.avatar,
+                            seguidores: userFound.seguidores,
+                            seguindo: userFound.seguindo,
+                            publicacoes: userFound.publicacoes,
+                        } as any;
+    
                         if (segueEsseUsuario && segueEsseUsuario.length > 0) {
-                            userFound.segueEsseUsuario = true;
-                        }else{
-                            userFound.segueEsseUsuario = false;
-                        }
-                        userFound.senha = null
+                            user.segueEsseUsuario = true;
+                        } 
+                        user.senha = null
+                        return user
                     });
-                    return res.status(200).json(usuariosEncontrados);
+                    return res.status(200).json(users);
                 }
             }
             return res.status(405).json({ erro: 'Metodo informado nao e valido' });
